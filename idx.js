c55b826babf3e3a681a79db930c51bf941b0d822
ianicakomputek <46000884+ianicakomputek@users.noreply.github.com>
2018-12-20 11:51:05 +0700

    Update idx.js

--- idx.js
+++ idx.js
@@ -5,7 +5,7 @@ function omenu_m(id,xarg1) {
          x.style.display = 'block';
          x.style.visibility = 'visible';
          x.style.opacity = '1.0';
-         document.getElementById('xtrigram').src = '/apl/gambar/close.png';
+         document.getElementById('xtrigram').src = 'https://raw.githubusercontent.com/ianicakomputek/pos/master/close.png';
          document.getElementById('xslide').style.display = 'none';
          document.getElementById('xrow').style.display = 'none';
          document.getElementById('xfooter').style.display = 'none';
@@ -14,7 +14,7 @@ function omenu_m(id,xarg1) {
          x.style.display = 'none';
          x.style.visibility = 'hidden';
          x.style.opacity = '0.0';
-         document.getElementById('xtrigram').src = '/apl/gambar/trigram.png';
+         document.getElementById('xtrigram').src = 'https://raw.githubusercontent.com/ianicakomputek/pos/master/trigram.png';
          document.getElementById('xslide').style.display = 'block';
          document.getElementById('xrow').style.display = 'block';
          document.getElementById('xfooter').style.display = 'block';
@@ -26,29 +26,12 @@ function omenu_m(id,xarg1) {
          x.style.display = 'none';
          x.style.visibility = 'hidden';
          x.style.opacity = '0.0';
-         document.getElementById('xtrigram').src = '/apl/gambar/trigram.png';
+         document.getElementById('xtrigram').src = 'https://raw.githubusercontent.com/ianicakomputek/pos/master/trigram.png';
          document.getElementById('xslide').style.display = 'block';
          document.getElementById('xrow').style.display = 'block';
          document.getElementById('xfooter').style.display = 'block';
       }
 }
-
-
-    /* Light YouTube Embeds by @labnol */
-    /* Web: http://labnol.org/?p=27941 */
-
-    document.addEventListener("DOMContentLoaded",
-        function() {
-            var div, n,
-                v = document.getElementsByClassName("youtube-player");
-            for (n = 0; n < v.length; n++) {
-                div = document.createElement("div");
-                div.setAttribute("data-id", v[n].dataset.id);
-                div.innerHTML = labnolThumb(v[n].dataset.id);
-                div.onclick = labnolIframe;
-                v[n].appendChild(div);
-            }
-        });
 
     function labnolThumb(id) {
         var thumb = '<img src="https://i.ytimg.com/vi/ID/hqdefault.jpg">',    <!--gak bisa dirubah nih ! -->
