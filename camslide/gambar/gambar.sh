29206294e67f1c8bcb7db161c170309900d052c6
ianicakomputek <46000884+ianicakomputek@users.noreply.github.com>
2018-12-25 22:47:30 +0700

    Add files via upload

---
+++ camslide/gambar/gambar.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+wget https://cdnjs.cloudflare.com/ajax/libs/Camera/1.3.4/images/blank.gif
+wget https://cdnjs.cloudflare.com/ajax/libs/Camera/1.3.4/images/camera_skins.png
+wget https://cdnjs.cloudflare.com/ajax/libs/Camera/1.3.4/images/camera-loader.gif
+wget https://cdnjs.cloudflare.com/ajax/libs/Camera/1.3.4/images/patterns/overlay1.png
+wget https://cdnjs.cloudflare.com/ajax/libs/Camera/1.3.4/images/patterns/overlay10.png
+wget https://cdnjs.cloudflare.com/ajax/libs/Camera/1.3.4/images/patterns/overlay2.png
+wget https://cdnjs.cloudflare.com/ajax/libs/Camera/1.3.4/images/patterns/overlay3.png
+wget https://cdnjs.cloudflare.com/ajax/libs/Camera/1.3.4/images/patterns/overlay4.png
+wget https://cdnjs.cloudflare.com/ajax/libs/Camera/1.3.4/images/patterns/overlay5.png
+wget https://cdnjs.cloudflare.com/ajax/libs/Camera/1.3.4/images/patterns/overlay6.png
+wget https://cdnjs.cloudflare.com/ajax/libs/Camera/1.3.4/images/patterns/overlay7.png
+wget https://cdnjs.cloudflare.com/ajax/libs/Camera/1.3.4/images/patterns/overlay8.png
+wget https://cdnjs.cloudflare.com/ajax/libs/Camera/1.3.4/images/patterns/overlay9.png
