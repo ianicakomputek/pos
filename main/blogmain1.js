2ef5c947f7b971233300a069b1103446cd7d04db
ianicakomputek <46000884+ianicakomputek@users.noreply.github.com>
2020-08-03 20:32:58 +0800

    Update blogmain1.js

--- main/blogmain1.js
+++ main/blogmain1.js
@@ -191,7 +191,14 @@
                      alert(xjson.jnote);
                   } else if (xjson.jstatus=="1") {
                      document.getElementById("formbase").style.display="none";
-                     document.getElementById("blokinfo").innerHTML=xjson.jnote;
+                     var ctx = document.getElementById("xcanvas").getContext('2d');
+                     ctx.font = '18px serif';
+                     ctx.fillText('Al-Izhar Cendekia - Makassar', 10, 50);
+                     ctx.fillText('PPDB Online', 20, 50);
+                     ctx.fillText('Nama Calon Murid', 30, 50);
+                     ctx.fillText(xu_name, 40, 50);
+                     ctx.fillText(xu_satuan, 50, 50);
+                     ctx.fillText(xu_tingkat, 60, 50);
                      document.getElementById("blokinfo").style.display="block";
                   }
                }
